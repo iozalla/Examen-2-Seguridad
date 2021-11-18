d8d601fece2aae2b97094b823f0f1b9dd1c63b16
iozalla <iozono11@gmail.com>
2021-11-18 19:14:42 +0100

    Añadido intruccion pa redireccionar

--- configurar SSL/crearlocalhost.sh
+++ configurar SSL/crearlocalhost.sh
@@ -37,6 +37,9 @@ configurarSSL(){
     sudo \cp -r ssl-params.conf /etc/apache2/conf-available/
     sudo \cp -r /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.bak #por si algo va mal
     echo -e "${RED}EN EL ARCHIVO /etc/apache2/sites-available/default-ssl.conf TIENES QUE EDITAR LAS LINEAS DE SERVER ADMIN Y SERVER NAME CON LO QUE PUSISTRE EN EL CERTIFICADO ${NC}"
+    echo -e '/etc/apache2/sites-available/000-default.conf añadir la linea Redirect "/" "TU IP"'
+    echo -e "${NC}"
+
     sudo \cp -r default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
     #sudo \cp -r 000-default.conf /etc/apache2/sites-available/000-default.conf
     #read -p "IP externa de tu server: " IP
